0ec79a86f8c27552dcd3b136fb17f9b2ed981b93
zhang make <zhangmake86@gmail.com>
2025-06-01 16:52:13 +0800

    updated output references

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "lambda_function_arn" {
-  value = aws_lambda_function.lambda_function.arn
+  value = module.lambda_function.function_arn
   description = "The ARN of the Lambda function"
 }
 
 output "lambda_function_name" {
-  value = aws_lambda_function.lambda_function.function_name
+  value = var.function_name
   description = "The name of the Lambda function"
 }
 
 output "lambda_function_invoke_arn" {
-  value = aws_lambda_function.lambda_function.invoke_arn
+  value = module.lambda_function.invoke_arn
   description = "The ARN to be used for invoking the Lambda function"
 }
 
 output "api_integration_id" {
-  value = aws_api_gateway_integration.lambda_integration.id
+  value = aws_apigatewayv2_integration.apigw_integration.id
 }
 
 output "api_route_id" {
-  value = aws_api_gateway_route.lambda_route.id
+  value = aws_apigatewayv2_route.apigw_route.id
   description = "The ID of the API route"
 }
 
